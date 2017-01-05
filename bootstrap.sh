b1e01e64578f8feda710892ba23dc6fcd2798e07
Pekka Astala <pekka.astala@sysart.fi>
2017-01-06 01:06:47 +0200

    Added mysql and apache2 setup to vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,16 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password tim
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password timelord'
 
 apt-get update
-apt-get install -y mysql-server php7.0 php-xml php-mysql php-mbstring zip unzip
+apt-get install -y mysql-server apache2 php7.0 libapache2-mod-php php-xml php-mysql php-mbstring zip unzip
+
+cat /vagrant/.env >> .bashrc
+cat /vagrant/.env >> /etc/apache2/envvars
 
 cd /vagrant && wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet
 
 service mysql start
+mysql -u root -ptimelord -e "create database quotesdb"
+
+rm -rf /var/www/html && ln -s /vagrant/public /var/www/html
+
+service apache2 restart
