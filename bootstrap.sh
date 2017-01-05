e681e861132e90aa11b7ff58432da580cf6e1bff
Pekka Astala <pekka.astala@sysart.fi>
2017-01-05 23:58:48 +0200

    Updated vagrantbox & php versions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y php5-cli zip
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password timelord'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password timelord'
 
-wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet
+apt-get update
+apt-get install -y mysql-server php7.0 php-xml php-mysql php-mbstring zip unzip
+
+cd /vagrant && wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet
+
+service mysql start
